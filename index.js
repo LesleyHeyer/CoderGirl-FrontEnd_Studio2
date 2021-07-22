9449fd0c6f3a4e88905c01c86479d42e40b52397
Lesley_Heyer <pingpongkat@yahoo.com>
2021-07-21 20:45:05 -0500

    first commit

--- index.js
+++ index.js
@@ -10,20 +10,45 @@ function getData() {
 
 //Function should return a component displaying an astronaut's bio
 function AstronautBios(astronaut) {
-
+  
   return (
-    <div>
-
-    </div>
+   <div className="astronaut">
+     <div className="bio">
+       <h3>{ astronaut.firstName } {astronaut.lastName}</h3>
+      <ul>
+        <li>Hours in Space: { astronaut.hoursInSpace }</li>
+        <li>Active: { astronaut.active === true ? "Active" : "Inactive" }</li>
+        <li>Skills: { astronaut.skills.join(', ') }</li>
+      </ul>
+     </div>
+     <img className="avatar" src={astronaut.picture}></img>
+   </div>
   )
+  
 }
 
-// Function should render all astronaut bios
+//Function should render all astronaut bios
 function renderBios(data) {
+  const astronautArray = [];
+
+  data.forEach( (astronaut) => {
+      astronautArray.push(AstronautBios(astronaut));
+  });
+
+
+
   //The AstronautBios component should be repeated to display bios for all 
   //astronaunts in the returned data object.
-  
+  const root = document.getElementById('root');
+  const container = 
+  <div className='container'>
+  {astronautArray}
+  </div>
+
+  ReactDOM.render( container, root );
   //Then attach to the 'root' div!
+
+
 }
 
 
